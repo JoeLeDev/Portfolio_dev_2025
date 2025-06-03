13c05b5df08fe78ccaa852af2da6e99575aae917
Jonathan Luembe <jonathanluembe@yahoo.com>
2025-06-03 15:32:54 +0200

    Dns Seo

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import HeadSEO from "./Seo";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <HeadSEO />
     <TooltipProvider>
       <Toaster />
       <Sonner />
