4596631e88df09207a50cfe3c9199b4e09af0152
Jonathan Luembe <joeledev@MacBook-Air-de-Jonathan.local>
2026-03-24 15:07:04 +0100

    fix: Links Header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,6 @@ const Header = () => {
     { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
     { name: 'Projects', href: '#projects' },
-    { name: 'Contact', href: '#contact' },
   ];
 
   return (
